ee41f542713ab0d2c52b065b44ab7c534d6c3829
Jonfras <jonas.kreuzh@gmail.com>
2022-09-22 17:22:47 +0200

    Interface Fields created

--- src/net/htlgkr/krejo/PrimeSieve/PrimeSieve.java
+++ src/net/htlgkr/krejo/PrimeSieve/PrimeSieve.java
@@ -1,5 +1,9 @@
 package net.htlgkr.krejo.PrimeSieve;
 
 public interface PrimeSieve {
+    final int Max = 20;//(int)Math.sqrt(max)+2;
+    boolean[] numbers = new boolean[Max];
+
+    int[] primes = new int[Max+1];
 
 }
