dccbfa2066491a40734a7fc28d67569590f99273
Jonfras <jonas.kreuzh@gmail.com>
2022-09-22 17:20:02 +0200

    Main Class and Interface created

---
+++ src/net/htlgkr/krejo/PrimeSieve/ErastosthenesPrimeSieve.java
@@ -0,0 +1,7 @@
+package net.htlgkr.krejo.PrimeSieve;
+
+public class ErastosthenesPrimeSieve {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/net/htlgkr/krejo/PrimeSieve/PrimeSieve.java
@@ -0,0 +1,5 @@
+package net.htlgkr.krejo.PrimeSieve;
+
+public interface PrimeSieve {
+
+}
