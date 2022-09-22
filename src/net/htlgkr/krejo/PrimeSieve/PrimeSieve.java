1a6655691a8f48c9a92dfd2c89c828e4377e0059
Jonfras <jonas.kreuzh@gmail.com>
2022-09-22 17:34:20 +0200

    implemented isPrime Method

--- src/net/htlgkr/krejo/PrimeSieve/PrimeSieve.java
+++ src/net/htlgkr/krejo/PrimeSieve/PrimeSieve.java
@@ -6,4 +6,21 @@ public interface PrimeSieve {
 
     int[] primes = new int[Max+1];
 
+    public static boolean isPrime(int p) {
+        int counter = 1;
+        primes[0] = 2;
+        if (p % 2 == 0) return false;
+        for (int prime = 3; prime + 2 < p; prime += 2) {
+            if (!numbers[prime]) {
+                primes[counter] = prime;
+                counter++;
+                for (int i = 2; prime * i < Max; i++) {
+                    numbers[prime * i] = true;
+                }
+            }
+
+        }
+        if (!numbers[p]) return true;
+        else return false;
+    }
 }
